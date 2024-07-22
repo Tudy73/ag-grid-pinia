76b0dc9846958e3f4b3972f97c22f22faa9bd9c2
tudor baraian <baraiantudor@yahoo.com>
2024-07-22 14:41:43 +0300

    take from dummy

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,24 @@ function onAddTodo() {
   rowData.value = [...rowData.value];
   newRow.value = "";
 }
-
+async function populateFromAPI(){
+  try{
+    const response = await fetch("https://dummyjson.com/c/9990-db19-4e7d-8e27")
+    const data = await response.json()
+    gridStore.$patch((state)=>{
+      state.rowData = data
+    })
+  }
+  catch(error){
+    console.error("Failed to fetch data: ",error)
+  }
+}
 </script>
 <template>
   <main>
     <input v-model="newRow" placeholder="Add new Todo">
     <button @click="onAddTodo()">Add Todo</button>
+    <button @click="populateFromAPI()">Get from API</button>
     <div class="ag-theme-alpine" style="height: 400px; width: 600px;">
       <ag-grid-vue
           style="width: 100%; height: 100%"
